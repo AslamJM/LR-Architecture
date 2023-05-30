4eb7a662cbae97767db6c2057853e1baf17d264a
AslamJM <jma8744@gmail.com>
2023-05-31 00:13:42 +0530

    added collapse navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import ThemeToggle from "./ui/Switch";
+import { MenuIcon, XIcon } from "lucide-react";
 
 const navlinks = [
-  { title: "Home", className: "mr-4", slug: "/" },
-  { title: "About", className: "mx-4", slug: "/about" },
-  { title: "Projects", className: "mx-4", slug: "/projects" },
-  { title: "Contact", className: "ml-4", slug: "/contact" },
+  { title: "Home", className: "mr-2 lg:mr-4", slug: "/" },
+  { title: "About", className: "mx-2 lg:mx-4", slug: "/about" },
+  { title: "Projects", className: "mx-2 lg:mx-4", slug: "/projects" },
+  { title: "Awards", className: "mx-2 lg:mx-4", slug: "/awards" },
+  { title: "Contact", className: "ml-2 lg:ml-4", slug: "/contact" },
 ];
 
 const Logo = () => {
@@ -17,10 +20,11 @@ const Logo = () => {
 
 const Navbar = () => {
   const pathname = usePathname();
+  const [show, setShow] = useState(false);
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center md:justify-between py-4">
+    <div className="flex items-center justify-between py-4">
       <Logo />
-      <ul className="flex items-center justify-center">
+      <ul className="hidden md:flex items-center justify-center">
         {navlinks.map((item) => (
           <li key={item.title} className={`${item.className} px-1 py-2`}>
             <Link
@@ -37,6 +41,40 @@ const Navbar = () => {
         ))}
       </ul>
       <ThemeToggle />
+      <button className="p-1 md:hidden rounded-full hover:bg-dark/10 dark:hover:bg-light/10 transition-all duration-300">
+        <MenuIcon
+          className="w-6 h-6  text-dark dark:text-light hover:text-primary dark:hover:text-primary"
+          onClick={() => setShow(true)}
+        />
+      </button>
+      <div
+        className={`absolute md:hidden w-[300px] h-full top-0 right-0 bg-primary opacity-95 z-10 ${
+          show ? "translate-x-[0px]" : "translate-x-[300px]"
+        } transition-all duration-500 `}
+      >
+        <button
+          className="absolute top-2 right-2 p-1 rounded-full hover:bg-dark/10 transition-colors duration-300"
+          onClick={() => setShow(false)}
+        >
+          <XIcon className="w-8 h-8 text-secondary hover:text-dark" />
+        </button>
+        <ul className="mt-32">
+          {navlinks.map((item) => (
+            <li key={item.title} className={` px-4 py-2`}>
+              <Link
+                href={item.slug}
+                className={`${
+                  pathname === item.slug
+                    ? "text-dark font-semibold"
+                    : "text-secondary"
+                }  hover:text-dark text-lg`}
+              >
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
--- src/components/ui/Switch.tsx
+++ src/components/ui/Switch.tsx
@@ -8,9 +8,9 @@ const ThemeToggle = () => {
   const { setTheme } = useTheme();
   return (
     <div className="flex items-center">
-      <Sun className="w-8 h-8 text-primary dark:text-light/50" />
+      <Sun className="w-6 h-6 text-primary dark:text-light/50" />
       <Switch.Root
-        className="w-[55px] h-[30px] bg-light/90 border-dark border-2 rounded-full mx-2 relative focus:shadow-[0_0_0_2px] focus:shadow-dark dark:focus:shadow-light data-[state=checked]:bg-dark/90 data-[state=checked]:border-light"
+        className="w-[55px] h-[25px] bg-light/90 border-dark/90 border-2 rounded-full mx-2 relative focus:shadow-[0_0_0_2px] focus:shadow-dark dark:focus:shadow-light data-[state=checked]:bg-dark/90 data-[state=checked]:border-light"
         onCheckedChange={(checked) => {
           if (checked) {
             setTheme("dark");
@@ -21,7 +21,7 @@ const ThemeToggle = () => {
       >
         <Switch.Thumb className="block w-[21px] h-[21px] rounded-full bg-primary shadow-sm shadow-blackA7 transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[29px]" />
       </Switch.Root>
-      <Moon className="w-8 h-8 text-dark/50 dark:text-primary" />
+      <Moon className="w-6 h-6 text-dark/50 dark:text-primary" />
     </div>
   );
 };
